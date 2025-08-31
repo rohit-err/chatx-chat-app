9364855d7acd29d6bfd52c8fa050f0bd684ade68
Rohit Kumar <rohit2004r82@gmail.com>
2025-09-01 02:39:17 +0530

    fixed cookie setup

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -6,10 +6,10 @@ export const generateToken = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, 
-    httpOnly: true, 
-    sameSite: "strict", 
-    secure: process.env.NODE_ENV !== "development",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "none", 
+    secure: true, 
   });
 
   return token;

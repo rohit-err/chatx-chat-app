5e09402c037439e40c19628c633328aee012433d
Rohit Kumar <rohit2004r82@gmail.com>
2025-09-01 04:11:12 +0530

    remove console

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
-console.log("MODE:", import.meta.env.MODE);
+
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "/api",
